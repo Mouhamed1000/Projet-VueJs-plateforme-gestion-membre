ecc1c4253964378b5c2ffcb544cf058bb3494352
Mouhamed1000 <ndoyenaby12@gmail.com>
2026-02-20 11:15:53 +0000

    Barre de menu ajouté !

---
+++ src/components/AppNavBar.vue
@@ -0,0 +1,23 @@
+<template>
+  
+<nav class="navbar navbar-expand-lg navbar-dark bg-white w-100">
+  <div class="container-fluid">
+    <a class="navbar-brand text-black fs-5 fw-bold" href="#">Consortium Jeunesse Sénégal</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
+      <div class="navbar-nav">
+        <a class="nav-link active text-dark" aria-current="page" href="#">Fonctionnalités</a>
+        <a class="nav-link text-dark" href="#">À propos</a>
+        <a class="nav-link text-dark" href="#">Contact</a>
+        <a class="nav-link disabled text-black" aria-disabled="true">Connexion</a>
+        <button class="btn text-white bg-success" type="submit">Inscription</button>
+      </div>
+    </div>
+  </div>
+</nav>
+
+<hr class="text-dark"/>
+
+</template>
